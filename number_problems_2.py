ca9fe23ea358de15e69ad0853bcbe79803c92c5a
samuelo23 <115102588+samuelo23@users.noreply.github.com>
2022-10-31 07:02:37 +0000

    Update number_problems_2.py

--- number_problems_2.py
+++ number_problems_2.py
@@ -2,7 +2,19 @@ import math
 
 
 def roman_numeral(n):
-	pass
+	m = ["", "M", "MM", "MMM"]
+	c = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM "]
+	x = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+	i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+	
+	thousands = m[num // 1000]
+	hundreds = c[(num % 1000) // 100]
+	tens = x[(num % 100) // 10]
+	ones = i[num % 10]
+	
+	ans = (thousands + hundreds + tens + ones)
+	
+	return ans
 
 def total_roman(n):
 	pass
